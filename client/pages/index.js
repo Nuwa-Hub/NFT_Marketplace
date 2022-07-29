cc9ba24bddaec644ba81145bfc7d24de491c57ac
Nuwa-Hub <you@example.com>
2022-07-30 05:04:13 +0530

    add

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Collection from "./collections/Collection";
 
 export default function Home() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	return <h1>sddfsd</h1>;
 }

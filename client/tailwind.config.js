1383a09ceae336fddd7c5c94756fde93adb70bfb
KalinduGandara <kalindugandara16@gmail.com>
2022-09-15 19:16:46 +0530

    bug fixed

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -20,32 +20,32 @@ module.exports = {
 
 			'2xl': '1536px',
 			// => @media (min-width: 1536px) { ... }
-		},
-		colors: {
-			transparent: 'transparent',
-			current: 'currentColor',
-			'white': '#ffffff',
-			'purple': '#3f3cbb',
-			'midnight': '#121063',
-			'metal': '#565584',
-			'tahiti': '#3ab7bf',
-			'silver': '#ecebff',
-			'bubble-gum': '#ff77e9',
-			'bermuda': '#78dcca',
-			'Fuchsia': '#d946ef',
-		},
-		boxShadow: {
-			'3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
-		},
-		borderWidth: {
-			DEFAULT: '1px',
-			'0': '0',
-			'2': '2px',
-			'3': '3px',
-			'4': '4px',
-			'6': '6px',
-			'10': '10px',
 		}
+	},
+	colors: {
+		transparent: 'transparent',
+		current: 'currentColor',
+		'white': '#ffffff',
+		'purple': '#3f3cbb',
+		'midnight': '#121063',
+		'metal': '#565584',
+		'tahiti': '#3ab7bf',
+		'silver': '#ecebff',
+		'bubble-gum': '#ff77e9',
+		'bermuda': '#78dcca',
+		'Fuchsia': '#d946ef',
+	},
+	boxShadow: {
+		'3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+	},
+	borderWidth: {
+		DEFAULT: '1px',
+		'0': '0',
+		'2': '2px',
+		'3': '3px',
+		'4': '4px',
+		'6': '6px',
+		'10': '10px',
 	},
 	plugins: [
 		require('@tailwindcss/forms'),

5e116fd381ec8d7e39ee1142addfe0cedfebe01b
Shilpa-Perera <laksahaniperera@gmail.com>
2022-09-16 13:14:21 +0530

    admin css

--- admin/pages/index.js
+++ admin/pages/index.js
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
+import NavBar from "../components/NavBar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-	return <h1>This is admin page</h1>;
+	return <NavBar />;
 }

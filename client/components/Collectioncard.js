616e3abb8f1c0c5fefc603d8e67e4b1a0a2c773b
Nuwa-Hub <you@example.com>
2022-09-22 03:44:12 +0530

    change collection explore page

--- client/components/Collectioncard.js
+++ client/components/Collectioncard.js
@@ -2,24 +2,20 @@ import React from 'react'
 
 const Collectioncard = () => {
   return (
-    <div class="nft" className=''>
+    <div className="nft">
     <div className="main flex flex-col p-4 w-full" >
       <img className="tokenImage rounded-lg h-64 max-w-full object-cover" src="/dog1.jpg" alt="NFT" />
-      <h2>Candlemass</h2>
+      
       <div className="tokenInfo flex items-center justify-between">
-        <div class="price">
-          <ins>$</ins>
-          <p>10.00 USD</p>
+        <div className="price">
+          <p className='flex items-center font-bold text-2xl'>Bored Api</p>
         </div>
-        <div class="duration">
-          <ins>Rarity:</ins>
+        <div className="duration absolute right-10 bottom-5">
+        <img className="tokenImage rounded-lg h-24 w-24 max-w-full object-cover relative ring-8 ring-neutral-50" src="/dog.jpg" alt="NFT" />
         </div>
       </div>
-      <div></div>
-      <div></div>
-      <hr />
-      <div class="creator">
-        <ins>Supply:</ins> 666
+      <div className="creator">
+        <p>Supply:</p> 666
       </div>
     </div>
   </div>

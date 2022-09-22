7a4efbd804cb5607e16c9a6d59b7ad5cef2d3765
Shilpa-Perera <laksahaniperera@gmail.com>
2022-09-23 00:33:46 +0530

    nft page

--- client/components/nft/Accordion.jsx
+++ client/components/nft/Accordion.jsx
@@ -1,21 +1,24 @@
 import Accordion from "react-bootstrap/Accordion";
 import ItemActivityTable from "./ItemActivityTable";
 import ListingTable from "./ListingTable";
+import PriceHistoryTable from "./PriceHistoryTable";
 const Accordion_ = () => {
 	return (
 		<div>
 			<Accordion defaultActiveKey="0">
 				<Accordion.Item eventKey="0">
 					<Accordion.Header>Price History</Accordion.Header>
-					<Accordion.Body></Accordion.Body>
+					<Accordion.Body>
+						<PriceHistoryTable />
+					</Accordion.Body>
 				</Accordion.Item>
-				<Accordion.Item eventKey="0">
+				<Accordion.Item eventKey="1">
 					<Accordion.Header>Listings</Accordion.Header>
 					<Accordion.Body>
 						<ListingTable />
 					</Accordion.Body>
 				</Accordion.Item>
-				<Accordion.Item eventKey="0">
+				<Accordion.Item eventKey="2">
 					<Accordion.Header>Item Activity</Accordion.Header>
 					<Accordion.Body>
 						<ItemActivityTable />

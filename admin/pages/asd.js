aafb6497be1bd95dec29d22367126abe5331f04a
KalinduGandara <kalindugandara16@gmail.com>
2022-09-22 22:57:09 +0530

    admin folder removed

--- admin/pages/asd.js
+++
@@ -1,8 +0,0 @@
-import CardSettings from "components/Cards/CardSettings";
-export default function asd() {
-    return (
-        <div>
-            <CardSettings />
-        </div>
-    )
-}
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,8 +1,10 @@
 import Navbar from "../components/Navbar";
-import "../styles/globals.css";
+import "styles/globals.css";
 import Head from "next/head";
 import UserLayout from "../layouts/UserLayout";
 import { MoralisProvider } from "react-moralis";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 
 function MyApp({ Component, pageProps }) {
   const Layout = Component.layout || UserLayout;

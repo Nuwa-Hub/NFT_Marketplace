f65428a3591a696904cda4baed3b467842becaf0
Nuwa-Hub <you@example.com>
2022-09-23 08:56:12 +0530

    change

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,6 @@
 import Navbar from "../components/Navbar";
 import "../styles/globals.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+//import "bootstrap/dist/css/bootstrap.min.css";
 import Head from "next/head";
 import Layout from "../components/Layout";
 import { MoralisProvider } from "react-moralis";

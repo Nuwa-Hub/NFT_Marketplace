b483e359ffa3dcbc9c2a40197f67be0ce0ef76cd
KalinduGandara <kalindugandara16@gmail.com>
2022-09-24 23:55:10 +0530

    reafac

--- client/redux/store.js
+++ client/redux/store.js
@@ -1,11 +1,9 @@
 
 import { configureStore } from '@reduxjs/toolkit'
 import collectionSlice from './slices/collectionSlice'
-import userSlice from './slices/userSlice'
 
 export const store = configureStore({
-  reducer: {
-    counter: collectionSlice,
-    user: userSlice,
-  },
+    reducer: {
+        collection: collectionSlice,
+      },
 })

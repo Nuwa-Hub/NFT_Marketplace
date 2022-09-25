53810a17b90e7d2214703b685ebea0a2d1071470
KalinduGandara <kalindugandara16@gmail.com>
2022-09-26 01:08:24 +0530

    add admin login func

---
+++ client/redux/actions/adminAction.js
@@ -0,0 +1,87 @@
+import axios from 'axios'
+import { createAsyncThunk } from '@reduxjs/toolkit'
+
+export const registerAdmin = createAsyncThunk(
+    'admin/register',
+    async ({ username, email, password, account, address, telNumber, img }, { rejectWithValue }) => {
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }
+            console.log(username, password, email, account, address, telNumber);
+            console.log('asdasdqweqweqwe');
+
+
+            await axios.post(
+                '/api/admin/add',
+                { username, email, password, account, address, telNumber, img },
+                config
+            )
+        } catch (error) {
+            if (error.response && error.response.data.message) {
+                return rejectWithValue(error.response.data.message)
+            } else {
+                return rejectWithValue(error.message)
+            }
+        }
+    }
+)
+
+export const getAdminDetails = createAsyncThunk(
+    'admin/getAdminDetails',
+    async (arg, { getState, rejectWithValue }) => {
+        try {
+            // get user data from store
+            const { user } = getState()
+
+            // configure authorization header with user's token
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${user.userToken}`,
+                },
+            }
+
+            const { data } = await axios.get(`/api/admin`, config)
+            return data
+        } catch (error) {
+            if (error.response && error.response.data.message) {
+                return rejectWithValue(error.response.data.message)
+            } else {
+                return rejectWithValue(error.message)
+            }
+        }
+    }
+)
+export const adminLogin = createAsyncThunk(
+    'admin/login',
+    async ({ email, password }, { rejectWithValue }) => {
+        try {
+            // configure header's Content-Type as JSON
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }
+
+            const { data } = await axios.post(
+                '/api/auth/adminlogin',
+                { email, password },
+                config
+            )
+
+            // store user's token in local storage
+            localStorage.setItem('adminToken', data.adminToken)
+
+            return data
+        } catch (error) {
+            // return custom error message from API if any
+            if (error.response && error.response.data.message) {
+                return rejectWithValue(error.response.data.message)
+            } else {
+                return rejectWithValue(error.message)
+            }
+        }
+    }
+)
--- client/redux/store.js
+++ client/redux/store.js
@@ -1,10 +1,12 @@
 
 import { configureStore } from '@reduxjs/toolkit'
+import adminSlices from './slices/adminSlices'
 import collectionSlice from './slices/collectionSlice'
 import userSlice from './slices/userSlice'
 export const store = configureStore({
   reducer: {
     collection: collectionSlice,
     user: userSlice,
+    admin: adminSlices,
   },
 })

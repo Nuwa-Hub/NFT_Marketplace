77abaa5b4f2b84b4088d9ef711cfc1ef428efd80
Shilpa-Perera <laksahaniperera@gmail.com>
2022-09-28 01:47:54 +0530

    accordion fixed

--- client/components/nft/Accordion.jsx
+++ client/components/nft/Accordion.jsx
@@ -1,11 +1,32 @@
-import Accordion from "react-bootstrap/Accordion";
+// import Accordion from "react-bootstrap/Accordion";
 import ItemActivityTable from "./ItemActivityTable";
 import ListingTable from "./ListingTable";
 import PriceHistoryTable from "./PriceHistoryTable";
 const Accordion_ = () => {
 	return (
 		<div>
-			<Accordion defaultActiveKey="0">
+			<div class="bg-white w-full border border-blue-300 divide-y divide-gray-200">
+				<details>
+					<summary class="question py-3 px-4 cursor-pointer select-none w-full outline-none">
+						Price History
+					</summary>
+					<PriceHistoryTable />
+				</details>
+				<details>
+					<summary class="question py-3 px-4 cursor-pointer select-none w-full">
+						Listings
+					</summary>
+					<ListingTable />
+				</details>
+				<details>
+					<summary class="question py-3 px-4 cursor-pointer select-none w-full">
+						Item Activity
+					</summary>
+					<ItemActivityTable />
+				</details>
+			</div>
+
+			{/* <Accordion defaultActiveKey="0">
 				<Accordion.Item eventKey="0">
 					<Accordion.Header>Price History</Accordion.Header>
 					<Accordion.Body>
@@ -24,7 +45,7 @@ const Accordion_ = () => {
 						<ItemActivityTable />
 					</Accordion.Body>
 				</Accordion.Item>
-			</Accordion>
+			</Accordion> */}
 		</div>
 	);
 };

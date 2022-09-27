40c2ed7c0c9c755275b7f38b8fc7af4250c92baa
Shilpa-Perera <laksahaniperera@gmail.com>
2022-09-27 23:58:20 +0530

    blah

--- client/redux/actions/NFTAction.js
+++ client/redux/actions/NFTAction.js
@@ -3,32 +3,42 @@ import { publicRequest, userRequest } from "/utils/requestMethods";
 
 //add NFT
 export const addNFTs = async (dispatch, newNFT) => {
-    try {
+	try {
+		const res = await publicRequest.post(`/nft`, newNFT);
 
-      const res = await publicRequest.post(`/nft`, newNFT);
-      
-      console.log(res.data)
-    } catch (err) {}
-  };
+		console.log(res.data);
+	} catch (err) {}
+};
 
-  //get all NFT
+//get all NFT
 export const getAllNFTs = async (dispatch) => {
-    try {
-      const res = await publicRequest.get(`/nft`);
-      //console.log(res.data)
-      dispatch(getNFTSuccess(res.data));
-    } catch (err) {
-        dispatch(getNFTFailure())
-    }
-  };
+	try {
+		const res = await publicRequest.get(`/nft`);
+		//console.log(res.data)
+		dispatch(getNFTSuccess(res.data));
+	} catch (err) {
+		dispatch(getNFTFailure());
+	}
+};
 
-    //get  NFT by collection id
-export const getNFTsByCollectionId = async (dispatch,collection_id) => {
-    try {
-      const res = await publicRequest.get(`/nft/collection/${collection_id}`);
-      //console.log(res.data)
-      dispatch(getNFTSuccess(res.data));
-    } catch (err) {
-        dispatch(getNFTFailure())
-    }
-  };
+//get  NFT by collection id
+export const getNFTsByCollectionId = async (dispatch, collection_id) => {
+	try {
+		const res = await publicRequest.get(`/nft/collection/${collection_id}`);
+		//console.log(res.data)
+		dispatch(getNFTSuccess(res.data));
+	} catch (err) {
+		dispatch(getNFTFailure());
+	}
+};
+
+//get  NFT by collection id
+export const getNFTsByNftId = async (dispatch, nft_id) => {
+	try {
+		const res = await publicRequest.get(`/nft/collection/${collection_id}`);
+		//console.log(res.data)
+		dispatch(getNFTSuccess(res.data));
+	} catch (err) {
+		dispatch(getNFTFailure());
+	}
+};

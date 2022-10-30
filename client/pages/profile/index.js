ec732c0b4958fb7c579cc649e5aee5d1c89afb88
KalinduGandara <kalindugandara16@gmail.com>
2022-10-30 21:25:07 +0530

    add

---
+++ client/pages/profile/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProfileCard from "components/ProfileCard";
+
+const index = () => {
+    return (
+        <div className=''>
+            <ProfileCard />
+        </div>
+    )
+}
+
+export default index

8db07b222b572530361d788946c1ed81d8ffe3ac
Shilpa-Perera <laksahaniperera@gmail.com>
2022-11-09 12:32:44 +0530

    Bid and Buy Modals

--- client/components/nft/BuyNowModal.jsx
+++ client/components/nft/BuyNowModal.jsx
@@ -1,4 +1,7 @@
-const ImageModal = () => {
+import { RiWallet3Fill } from "react-icons/ri";
+import { useState } from "react";
+const BuyNowModal = () => {
+	// const [modal, setModal] = useState(true);
 	return (
 		<div>
 			<div
@@ -14,38 +17,29 @@ const ImageModal = () => {
 						<div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
 							<div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
 								<div class="sm:flex sm:items-start">
-									<div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-										<svg
-											class="h-6 w-6 text-red-600"
-											xmlns="http://www.w3.org/2000/svg"
-											fill="none"
-											viewBox="0 0 24 24"
-											stroke-width="1.5"
-											stroke="currentColor"
-											aria-hidden="true"
-										>
-											<path
-												stroke-linecap="round"
-												stroke-linejoin="round"
-												d="M12 10.5v3.75m-9.303 3.376C1.83 19.126 2.914 21 4.645 21h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 4.88c-.866-1.501-3.032-1.501-3.898 0L2.697 17.626zM12 17.25h.007v.008H12v-.008z"
-											/>
-										</svg>
+									<div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
+										<RiWallet3Fill />
 									</div>
 									<div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
 										<h3
 											class="text-lg font-medium leading-6 text-gray-900"
 											id="modal-title"
 										>
-											Deactivate account
+											Buy Now
 										</h3>
+										<div className="flex py-2">
+											Sale Ends in: 1d 2h 3m 4s
+										</div>
+										<div className="flex py-2">
+											Current price : 2.325 ETH
+										</div>
 										<div class="mt-2">
-											<p class="text-sm text-gray-500">
-												Are you sure you want to
-												deactivate your account? All of
-												your data will be permanently
-												removed. This action cannot be
-												undone.
-											</p>
+											<input
+												class="shadow appearance-none border rounded w-full py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+												id="bid price"
+												type="text"
+												placeholder="Price"
+											/>
 										</div>
 									</div>
 								</div>
@@ -53,12 +47,18 @@ const ImageModal = () => {
 							<div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
 								<button
 									type="button"
-									class="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+									onClick={() => {
+										console.log("clicked bid");
+									}}
+									class="inline-flex w-full justify-center rounded-md border border-transparent bg-green-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
 								>
-									Deactivate
+									Buy Now
 								</button>
 								<button
 									type="button"
+									onClick={() => {
+										console.log("clicked cancel");
+									}}
 									class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
 								>
 									Cancel
@@ -72,4 +72,4 @@ const ImageModal = () => {
 	);
 };
 
-export default ImageModal;
+export default BuyNowModal;

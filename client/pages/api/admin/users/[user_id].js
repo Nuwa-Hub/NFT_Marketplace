f6133896d97dc4c8268f387615c7c73052dc4f7e
KalinduGandara <kalindugandara16@gmail.com>
2022-11-16 00:36:28 +0530

    admin user block

--- client/pages/api/admin/users/[user_id].js
+++ client/pages/api/admin/users/[user_id].js
@@ -2,6 +2,7 @@ import User from "models/User";
 import Collection from "models/Collection";
 import NFT from "models/NFT";
 import connectDB from "utils/connectDB";
+const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 
 export default async function handler(req, res) {
@@ -36,8 +37,55 @@ export default async function handler(req, res) {
                 }
             ])
             if (user[0]) {
-                console.log(user[0])
-                return res.status(200).json({ user: user[0] });
+                user = user[0];
+                let auctionAndBids = await User.aggregate([
+                    {
+                        '$match': {
+                            'walletAdress': user_id
+                        }
+                    }, {
+                        '$lookup': {
+                            'from': 'bids',
+                            'localField': 'walletAdress',
+                            'foreignField': 'bidder',
+                            'as': 'bids'
+                        }
+                    }, {
+                        '$unwind': {
+                            'path': '$bids',
+                            'preserveNullAndEmptyArrays': true
+                        }
+                    }, {
+                        '$lookup': {
+                            'from': 'auctions',
+                            'localField': 'bids.auctionId',
+                            'foreignField': '_id',
+                            'as': 'auction'
+                        }
+                    }, {
+                        '$unwind': {
+                            'path': '$auction',
+                            'preserveNullAndEmptyArrays': true
+                        }
+                    }, {
+                        '$group': {
+                            '_id': '$_id',
+                            'walletAdress': {
+                                '$first': '$walletAdress'
+                            },
+                            'auctions': {
+                                '$push': '$auction'
+                            },
+                            'bids': {
+                                '$push': '$bids'
+                            }
+                        }
+                    }
+                ])
+                user.auctions = auctionAndBids[0].auctions;
+                user.bids = auctionAndBids[0].bids;
+                // console.log(user)
+                return res.status(200).json({ user });
             } else {
                 return res.status(404).json({ success: false, message: "User not found" });
             }
@@ -48,6 +96,21 @@ export default async function handler(req, res) {
         // } else {
         //     res.status(401).json({ message: "Not authorized" });
         // }
+    }
+    if (req.method === "PUT") {
+        try {
+            const newUser = { ...req.body };
+
+            const updateUser = await User.findByIdAndUpdate(
+                new mongoose.Types.ObjectId(user_id),
+                { $set: newUser },
+                { new: true }
+            );
+            res.status(200).json(updateUser);
+        } catch (err) {
+            console.log(err)
+            res.status(500).json(err);
+        }
     }
     // res.status(400).json({ message: "Invalid request" });
 }
